2a139a0c4b91d57211602a8232e5a6d6f38cd2e1
citeb.odilon <odilon@citeb.com.br>
2022-12-21 22:32:50 -0500

    feat: create a scripts to docker run

---
+++ scripts/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -z "$BASEDIR" ]
+then
+    BASEDIR=$(pwd)
+fi
+
+case "$1" in
+        up)
+                echo "Up Daemon ..."
+                DUID=$(id -u) DGID=$DOCKER_GROUP_ID  docker-compose -f $(pwd)/docker/docker-compose-test.yml down;
+                DUID=$(id -u) DGID=$(id -g) docker-compose -f $BASEDIR/docker/docker-compose-test.yml up \
+                --abort-on-container-exit  --exit-code-from devices-service-test;
+                ;;
+        shell)
+                echo "Up ..."
+                DUID=$(id -u) DGID=$(id -g) docker-compose -f $BASEDIR/docker/docker-compose-test.yml run notifications-service-test
+                ;;
+        down)
+                echo "Down ..."
+                DUID=$(id -u) DGID=$(id -g) docker-compose -f $BASEDIR/docker/docker-compose-test.yml down -v
+                ;;
+        *)
+                echo $"Usage: $0 {upd|shell|down}"
+                RETVAL=2
+esac
+exit $RETVAL
